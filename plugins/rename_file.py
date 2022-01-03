105ff388842025d96bb35e387fa520570f8dba38
fpband <82159763+fpband@users.noreply.github.com>
2022-01-03 11:08:31 +0330

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -42,7 +42,7 @@ async def rename_doc(bot, update):
                return
         except UserNotParticipant:
             await update.reply_text(text=Scripted.JOIN_NOW_TEXT,
-                  reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton(text="ᴊᴏɪɴ ɴᴏᴡ 🔓", url=f"https://t.me/{Config.UPDATE_CHANNEL}") ]
+                  reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton(text="🌸 عضویت 🌸", url=f"https://t.me/{Config.UPDATE_CHANNEL}") ]
                 ] 
               )
             )
